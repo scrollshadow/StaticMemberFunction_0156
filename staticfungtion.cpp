1b1e0ccb39e063402468830fbd50c7fc5817c91a
scrollshadow <zaldhypramana@gmail.com>
2026-05-04 16:04:12 +0700

    10

--- staticfungtion.cpp
+++ staticfungtion.cpp
@@ -48,7 +48,7 @@ int main() {
     //memberi nilai pada setNim() untuk merubah nilai NIM
     Mahasiswa::setNim(5);
     Mahasiswa mhs3("Andi");
-    Mahasiswa mhs4("Joko Wahono");
+    Mahasiswa mhs4("Joko");
 
     //memanggil prosedur printAll()
     mhs1.printAll();
