9f64d6a0df28325e621b1bf1e9079638d28dddfe
scrollshadow <zaldhypramana@gmail.com>
2026-05-04 16:07:41 +0700

    12

--- construktorberparameter.cpp
+++ construktorberparameter.cpp
@@ -31,7 +31,7 @@ void mahasiswa::printAll() {
 int main() {
     mahasiswa mhs1("Lia");
     mahasiswa mhs2("Asroni");
-    mahasiswa mhs3("Andi Jalu");
+    mahasiswa mhs3("Andi");
     mahasiswa mhs4("Joko Purbo");
 
     mhs1.printAll();
