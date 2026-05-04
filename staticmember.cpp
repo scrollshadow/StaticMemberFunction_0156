bd25ecf683788ef60b52df171cd7c0e6e41198ee
scrollshadow <zaldhypramana@gmail.com>
2026-05-04 16:12:09 +0700

    18

--- staticmember.cpp
+++ staticmember.cpp
@@ -34,7 +34,7 @@ void Mahasiswa::printAll() {
 
 int main() {
     //membuat object dan memberi nilai
-    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs1("Lia");
     Mahasiswa mhs2("Asroni");
     Mahasiswa mhs3("Andi Kurniawan");
     Mahasiswa mhs4("Joko Purbo");
